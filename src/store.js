f39c50eca8a03513c6e7b4585d72304ca55bba4b
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-07-10 19:25:11 +0200

    feat: export default store

--- src/store.js
+++ src/store.js
@@ -7,4 +7,5 @@ const rootReducer = combineReducers({
   customer: customerReducer,
 });
 
-export const store = createStore(rootReducer);
+const store = createStore(rootReducer);
+export default store;
