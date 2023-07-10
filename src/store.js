881e63216db64594c712e3f5ca63d9b2c1b6e5c9
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-07-10 12:38:29 +0200

    feat: install redux, use createStore with reducer fuction

--- src/store.js
+++ src/store.js
@@ -1,3 +1,5 @@
+import { createStore } from "redux";
+
 const initialState = {
   balance: 0,
   loan: 0,
@@ -33,3 +35,5 @@ function reducer(state = initialState, action) {
       return state;
   }
 }
+
+const store = createStore(reducer);
